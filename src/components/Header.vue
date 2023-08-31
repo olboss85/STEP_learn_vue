494e4dadb210c16849979cc57c23f5565a6a73da
Olga <olboss85@gmail.com~>
2023-08-31 22:50:21 +0600

    Header

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+
+<script setup>
+</script>
+
+<template>
+  <h1 class="header-title">My Vue Project</h1>
+</template>
+
+<style scoped>
+  .header-title {
+    color: grey;
+    font-size: 24px;
+    width: 1170px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #e6f1ee;
+  }
+</style>
