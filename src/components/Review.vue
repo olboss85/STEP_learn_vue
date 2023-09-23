9dc300a7416cafae34a44a7bdef3b8d8a31cd211
Olga <olboss85@gmail.com~>
2023-09-23 17:13:53 +0600

    final reviewage

--- src/components/Review.vue
+++ src/components/Review.vue
@@ -1,14 +1,36 @@
 <template>
-    <!-- <div>
-        <h1>Отзывы</h1>
-        <div class="card" v-for="review in reviews" :key="index">
-          <h3>{{ review.name }}</h3>
-          <p>{{ review.phone }}</p>
-          <p>{{ review.text }}</p>
-        </div>
-      </div> -->
+  <div class="card">
+    <h1>Отзывы</h1>
+    <div v-for="review in reviews" :key="review.id">
+      <Panel :header="`Отзыв пользователя: ` + review.name" toggleable>
+        <p>{{ review.text }}</p>
+      </Panel>
+    </div>
+  </div>
 </template>
 
 <script setup>
-// import { reviews } from '../composable/reviews';
+import Panel from 'primevue/panel';
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '@/firebases'
+import { ref, onMounted } from 'vue';
+
+const reviews = ref([]);
+
+onMounted(async () => {
+    try {
+        const querySnapshot = await getDocs(collection(db, 'reviews'))
+
+        querySnapshot.forEach((doc) => {
+            reviews.value.push({
+                id: doc.id,
+                ...doc.data(),
+            });
+        });
+    } catch (error) {
+        console.error('Error getting reviews:', error);
+    }
+});
+
+
 </script>
--- src/views/ReviewPage.vue
+++ src/views/ReviewPage.vue
@@ -1,51 +1,9 @@
 <template>
-  <div class="card">
-    <h1>Отзывы</h1>
-    <div v-for="review in reviews" :key="review.id">
-      <Panel :header="`Отзыв пользователя: ` + review.name" toggleable>
-        <p>{{ review.text }}</p>
-      </Panel>
-    </div>
-  </div>
+<Review />
 </template>
 
-<!-- 
-
-
-<template>
-  <div class="card">
-    <h1>Отзывы</h1>
-    <div class="card" v-for="review in reviews" :key="review">
-      <h3>{{ review.name }}</h3>
-      <p>{{ review.phone }}</p>
-      <p>{{ review.text }}</p>
-  </div>
-
-</template> -->
-
 <script setup>
-// import Review from '@/components/Review.vue'
-import Panel from 'primevue/panel';
-import { collection, getDocs } from 'firebase/firestore'
-import { db } from '@/firebases'
-import { ref, onMounted } from 'vue';
-
-const reviews = ref([]);
-
-onMounted(async () => {
-    try {
-        const querySnapshot = await getDocs(collection(db, 'reviews'))
-
-        querySnapshot.forEach((doc) => {
-            reviews.value.push({
-                id: doc.id,
-                ...doc.data(),
-            });
-        });
-    } catch (error) {
-        console.error('Error getting reviews:', error);
-    }
-});
+import Review from '@/components/Review.vue'
 </script>
 
 <style scoped>
